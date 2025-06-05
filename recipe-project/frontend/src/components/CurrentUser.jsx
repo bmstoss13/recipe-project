eddf9d013240056601581b31eefc34c174a7d6b3
myngbruh <71903484+myngbruh@users.noreply.github.com>
2025-06-05 07:27:13 -0700

    fixed navbar view

--- recipe-project/frontend/src/components/CurrentUser.jsx
+++ recipe-project/frontend/src/components/CurrentUser.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import { auth, db } from '../firebase';
